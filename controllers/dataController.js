21e554a4774c8bf165cf66335f4611d1a670a378
Aman8683 <programmer.8081@gmail.com>
2024-07-04 10:49:59 +0530

    minor change

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,12 +1,8 @@
 const dataModel = require("../models/dataModel");
 
 const emissionDataHandler = (jsonData) => {
-  if (jsonData) {
-    dataModel.writeDataToExcel(jsonData);
-    console.log("Data written to Excel file successfully.");
-  } else {
-    console.error("Invalid data");
-  }
+   const status =  dataModel.writeDataToExcel(jsonData);
+    console.log(status);
 };
 
 module.exports = {
